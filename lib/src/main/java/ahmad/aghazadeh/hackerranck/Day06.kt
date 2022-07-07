d621ec65b9dea4cdcb485cdbc053b0040db39281
Ahmad Aghazadeh <890683@DBI.COM>
2022-07-07 15:31:49 +0430

    Add Day05

---
+++ lib/src/main/java/ahmad/aghazadeh/hackerranck/Day06.kt
@@ -0,0 +1,13 @@
+package ahmad.aghazadeh.hackerranck
+
+
+fun main(args: Array<String>) {
+    callDay06(2)
+}
+
+fun callDay06(multiplier:Int){
+    for (i in 1..10){
+        println("$multiplier x $i = ${multiplier*i}")
+    }
+}
+
