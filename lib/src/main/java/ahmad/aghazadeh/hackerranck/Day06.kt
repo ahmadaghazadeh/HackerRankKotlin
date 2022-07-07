0bde7071d9b780a337af65863fb62aba36dad447
Ahmad Aghazadeh <890683@DBI.COM>
2022-07-07 15:58:28 +0430

    Add Day06

--- lib/src/main/java/ahmad/aghazadeh/hackerranck/Day06.kt
+++ lib/src/main/java/ahmad/aghazadeh/hackerranck/Day06.kt
@@ -2,12 +2,20 @@ package ahmad.aghazadeh.hackerranck
 
 
 fun main(args: Array<String>) {
-    callDay06(2)
+    callDay06("Hacker")
+    callDay06("Rank")
 }
 
-fun callDay06(multiplier:Int){
-    for (i in 1..10){
-        println("$multiplier x $i = ${multiplier*i}")
+fun callDay06(text:String){
+    var evenIndexed=""
+    var oddIndexed=""
+    for (i in text.indices){//i in 0 until text.length
+        if(i%2==0){
+            evenIndexed += text[i]
+        }else{
+            oddIndexed += text[i]
+        }
     }
+    println("$evenIndexed $oddIndexed")
 }
 
