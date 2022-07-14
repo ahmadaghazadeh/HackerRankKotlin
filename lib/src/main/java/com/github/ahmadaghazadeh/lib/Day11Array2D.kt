79d2991a6087bfe409be28a0465ff672003fe5ff
Ahmad Aghazadeh <890683@DBI.COM>
2022-07-14 11:15:40 +0430

    Add day11

--- lib/src/main/java/com/github/ahmadaghazadeh/lib/Day11Array2D.kt
+++ lib/src/main/java/com/github/ahmadaghazadeh/lib/Day11Array2D.kt
@@ -12,9 +12,9 @@ fun main() {
         arrayOf(1, 1, 1, 0, 0, 0),
         arrayOf(0, 1, 0 ,0 ,0, 0),
         arrayOf(1, 1, 1, 0, 0, 0),
-        arrayOf(0, 0 ,2 ,4 ,4 ,0),
-        arrayOf(0, 0, 0, 2 ,0, 0),
-        arrayOf(0, 0, 1, 2, 4, 0),
+        arrayOf(0, 9 ,2 ,-4 ,-4 ,0),
+        arrayOf(0, 0, 0, -2 ,0, 0),
+        arrayOf(0, 0, -1, -2, -4, 0),
     )
     val maxSumHourGlass=getMaxSumHourGlass(arrayHourGlass)
     print(maxSumHourGlass)
@@ -23,8 +23,8 @@ fun main() {
 fun getMaxSumHourGlass(arrayHourGlass: Array<Array<Int>>): Int {
 
     var maxSumHourGlass=0
-    for (rowIndex in 0..arrayHourGlass.size-2){
-        for (colIndex in 0..arrayHourGlass[rowIndex].size-2)
+    for (rowIndex in 0..arrayHourGlass.size-3){
+        for (colIndex in 0..arrayHourGlass[rowIndex].size-3)
         {
             val sumHourGlass=getSumHourGlass(arrayHourGlass,rowIndex,colIndex)
             if (maxSumHourGlass<sumHourGlass)
@@ -39,10 +39,14 @@ fun getMaxSumHourGlass(arrayHourGlass: Array<Array<Int>>): Int {
 fun getSumHourGlass(arrayHourGlass: Array<Array<Int>>, row: Int, col: Int): Int {
 
     var sumHourGlass=0
-    for (rowIndex in 0..row){
-        for (colIndex in 0..col)
+    for (rowIndex in row..row+2){
+        for (colIndex in col..col+2)
         {
-            sumHourGlass+=arrayHourGlass[rowIndex][colIndex]
+            if(rowIndex==row || rowIndex==row+2){
+                sumHourGlass+=arrayHourGlass[rowIndex][colIndex]
+            }else if(rowIndex==row+1 && colIndex==col+1){
+                sumHourGlass+=arrayHourGlass[rowIndex][colIndex]
+            }
         }
     }
     return sumHourGlass
