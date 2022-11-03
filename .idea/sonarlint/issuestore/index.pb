ab1886fac567739348d89907f30f078dfcd57a12
Ahmad Aghazadeh <890683@DBI.COM>
2022-11-03 08:19:29 +0330

    Add Day17

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,11 @@ Xapp/src/androidTest/java/com/github/ahmadaghazadeh/hackerrank/ExampleInstrument
 app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
 t
 Dlib/src/main/java/com/github/ahmadaghazadeh/lib/AverageDailySales.kt,3\3\33064b6760af4d2c13fc1724f6f5ef5ad84dbef6
+n
+>lib/src/main/java/com/github/ahmadaghazadeh/lib/Day06String.kt,e\b\eb33d6b9641e20878989a271414c213ce31d0461
+s
+Clib/src/main/java/com/github/ahmadaghazadeh/lib/Day03Conditional.kt,0\0\00e4d8269421394d208e7ae9680d4010fb14fa32
+n
+>lib/src/main/java/com/github/ahmadaghazadeh/lib/Day10Binary.kt,2\c\2c44d69ad7686c1553fe5d90c92ce99a6f6f2ded
+m
+=lib/src/main/java/com/github/ahmadaghazadeh/lib/Day05Loops.kt,3\f\3f21a0b389f1cfbc4f7faf22fcf86749ba4c42d3
---
+++ lib/src/main/java/com/github/ahmadaghazadeh/lib/Day17MoreExceptions.kt
@@ -0,0 +1,44 @@
+package com.github.ahmadaghazadeh.lib
+
+import java.lang.Exception
+
+// Solution 1 :
+// To find max and mix number in array and Abs(max-min)
+
+// Solution 2 :
+// Sort the array then last element - first element
+fun main() {
+    callDay17();
+}
+fun callDay17(){
+    val inputs = arrayOf(
+        arrayOf(3, 5),
+        arrayOf(3, 4),
+        arrayOf(-1, -2),
+        arrayOf(-1, 3),
+    )
+    val calculator=Calculator()
+    for (i in inputs.indices){
+        try {
+            println(calculator.power(inputs[i][0],inputs[i][1]))
+        }catch (ex:Exception){
+            println(ex.message)
+        }
+    }
+
+}
+class Calculator{
+    fun power(n:Int,p:Int):Int{
+        if(n<0 || p<0){
+            throw Exception("n and p should be non-negative.")
+        }
+        var result=1;
+        for(i in 0 until p){
+            result *= n;
+        }
+        return result;
+    }
+}
+
+
+
